b79c3726d5c5c4e0135cfb4896bce41a1d292170
GuiltyMaiden <119522800+GuiltyMaiden@users.noreply.github.com>
2024-08-02 18:10:52 -0300

    App_resumo.py

--- App_resumo.py
+++ App_resumo.py
@@ -1,19 +1,20 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-
-import streamlit as st
 from sumy.parsers.plaintext import PlaintextParser
 from sumy.nlp.tokenizers import Tokenizer
 from sumy.summarizers.lsa import LsaSummarizer
 
-
 def summarize_text(text, sentence_count=3):
-    parser = PlaintextParser.from_string(text, Tokenizer("english"))
-    summarizer = LsaSummarizer()
-    summary = summarizer(parser.document, sentence_count)
-    return " ".join([str(sentence) for sentence in summary])
-
+    try:
+        # Inicializar o parser e o summarizer
+        parser = PlaintextParser.from_string(text, Tokenizer("english"))
+        summarizer = LsaSummarizer()
+        summary = summarizer(parser.document, sentence_count)
+        return " ".join([str(sentence) for sentence in summary])
+    except LookupError as e:
+        st.error(f"Ocorreu um erro ao processar o texto: {e}")
+        return "Erro ao processar o texto."
 
 st.title("App de Resumo de Textos com IA")
 
@@ -23,7 +24,6 @@ text = st.text_area("", height=200)
 if st.button("Gerar Resumo"):
     if text:
         summary = summarize_text(text)
-
         st.subheader("Resumo:")
         st.write(summary)
     else:
@@ -31,3 +31,4 @@ if st.button("Gerar Resumo"):
 
 st.markdown(
     "Este app usa a biblioteca [sumy](https://github.com/miso-belica/sumy) para sumarização de texto.")
+
